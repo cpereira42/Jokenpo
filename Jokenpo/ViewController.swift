0ca12588c1182a7e80531f85b85ac30e109e4d1c
Cezar <cezaracp1@gmail.com>
2020-04-07 21:27:03 -0700

    Initial Commit

---
+++ Jokenpo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jokenpo
+//
+//  Created by Cezar Pereira on 07/04/20.
+//  Copyright © 2020 Cezar Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JokenpoTests/JokenpoTests.swift
@@ -0,0 +1,34 @@
+//
+//  JokenpoTests.swift
+//  JokenpoTests
+//
+//  Created by Cezar Pereira on 07/04/20.
+//  Copyright © 2020 Cezar Pereira. All rights reserved.
+//
+
+import XCTest
+@testable import Jokenpo
+
+class JokenpoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
